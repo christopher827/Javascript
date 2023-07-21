91c7448b4317dda1ebb7088429c5fc6b39ddc3ad
Christopher Oche <104537376+christopher827@users.noreply.github.com>
2023-07-21 13:37:42 +0100

    Update Clock.js

--- Clock/Clock.js
+++ Clock/Clock.js
@@ -16,18 +16,3 @@ else{
 
 }
 setInterval(dispayTime,10);
-/*
-function DisplayTime(){
-    var dateTime=new Date();
-    var hrs=dateTime.getHours();
-    var min=dateTime.getMinutes();
-    var sec=dateTime.getSeconds();
-    document.getElementById('sessions');
-
-    document.getElementById('hours').innerHTML=hrs;
-    document.getElementById('minutes').innerHTML=minutes;
-    document.getElementById('sec').innerHTML=sec;
-
-
-}
-setInterval(DisplayTime,10);*/
